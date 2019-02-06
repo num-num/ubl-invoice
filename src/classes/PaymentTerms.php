15ca2380966bff6e3d30cd03bd055dfa1b98b50c
Bert Devriese <bert@builtinbruges.com>
2019-02-06 12:10:49 +0100

    Add support for cbc:SettlementDiscountPercent and cac:SettlementPeriod

--- src/classes/PaymentTerms.php
+++ src/classes/PaymentTerms.php
@@ -8,6 +8,9 @@ use Sabre\Xml\XmlSerializable;
 class PaymentTerms implements XmlSerializable
 {
 	private $note;
+	private $settlementDiscountPercent;
+	private $amount;
+	private $settlementPeriod;
 
 	/**
 	 * @return mixed
@@ -19,7 +22,7 @@ class PaymentTerms implements XmlSerializable
 
 	/**
 	 * @param mixed $note
-	 * @return PaymentTerms
+	 * @return String
 	 */
 	public function setNote($note)
 	{
@@ -27,8 +30,84 @@ class PaymentTerms implements XmlSerializable
 		return $this;
 	}
 
+	/**
+	 * @return mixed
+	 */
+	public function getSettlementDiscountPercent()
+	{
+		return $this->settlementDiscountPercent;
+	}
+
+	/**
+	 * @param mixed $settlementDiscountPercent
+	 * @return float
+	 */
+	public function setSettlementDiscountPercent($settlementDiscountPercent)
+	{
+		$this->settlementDiscountPercent = $settlementDiscountPercent;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getAmount()
+	{
+		return $this->amount;
+	}
+
+	/**
+	 * @param mixed $amount
+	 * @return float
+	 */
+	public function setAmount($amount)
+	{
+		$this->amount = $amount;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSettlementPeriod()
+	{
+		return $this->settlementPeriod;
+	}
+
+	/**
+	 * @param mixed $settlementPeriod
+	 * @return SettlementPeriod  <?=$t->g('translation_key');?>
+	 */
+	public function setSettlementPeriod(SettlementPeriod $settlementPeriod)
+	{
+		$this->settlementPeriod = $settlementPeriod;
+		return $this;
+	}
+
 	function xmlSerialize(Writer $writer)
 	{
-		$writer->write([ Schema::CBC . 'Note' => $this->note ]);
+		if ($this->note !== null) {
+			$writer->write([ Schema::CBC . 'Note' => $this->note ]);
+		}
+
+		if ($this->settlementDiscountPercent != null) {
+			$writer->write([ Schema::CBC . 'SettlementDiscountPercent' => $this->settlementDiscountPercent ]);
+		}
+
+		if ($this->amount != null) {
+			$writer->write([
+				[
+					'name' => Schema::CBC . 'Amount',
+					'value' => number_format($this->amount, 2, '.', ''),
+					'attributes' => [
+						'currencyID' => 'EUR'
+					]
+				]
+			]);
+		}
+
+		if ($this->settlementPeriod != null) {
+			$writer->write([ Schema::CAC . 'SettlementPeriod' => $this->settlementPeriod ]);
+		}
 	}
 }
---
+++ src/classes/SettlementPeriod.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace NumNum\UBL;
+
+use Sabre\Xml\Writer;
+use Sabre\Xml\XmlSerializable;
+
+class SettlementPeriod implements XmlSerializable
+{
+	private $startDate;
+	private $endDate;
+
+	/**
+	 * @return mixed
+	 */
+	public function getStartDate()
+	{
+		return $this->startDate;
+	}
+
+	/**
+	 * @param mixed $startDate
+	 * @return DateTime
+	 */
+	public function setStartDate(\DateTime $startDate)
+	{
+		$this->startDate = $startDate;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getEndDate()
+	{
+		return $this->endDate;
+	}
+
+	/**
+	 * @param mixed $endDate
+	 * @return DateTime
+	 */
+	public function setEndDate(\DateTime $endDate)
+	{
+		$this->endDate = $endDate;
+		return $this;
+    }
+
+	/**
+	 * The validate function that is called during xml writing to valid the data of the object.
+	 *
+	 * @throws InvalidArgumentException An error with information about required data that is missing to write the XML
+	 * @return void
+	 */
+	function validate()
+	{
+        if ($this->startDate === null)
+        {
+			throw new \InvalidArgumentException('Missing startDate');
+        }
+        if ($this->endDate === null)
+        {
+			throw new \InvalidArgumentException('Missing endDate');
+        }
+    }
+
+	/**
+	 * The xmlSerialize method is called during xml writing.
+	 *
+	 * @param Writer $writer
+	 * @return void
+	 */
+	function xmlSerialize(Writer $writer)
+	{
+        $this->validate();
+
+		$writer->write([
+			Schema::CBC . 'StartDate' => $this->startDate->format('Y-m-d'),
+			Schema::CBC . 'EndDate' => $this->endDate->format('Y-m-d'),
+        ]);
+
+		$writer->write([
+			[
+				'name' => Schema::CBC . 'DurationMeasure',
+				'value' => $this->endDate->diff($this->startDate)->format('%d'),
+				'attributes' => [
+					'unitCode' => 'DAY'
+				]
+			]
+		]);
+	}
+}
