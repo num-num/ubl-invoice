cfb27be24bb89a96c68f2e06b603064a9b458cef
Bert Devriese <bert@numnum.be>
2023-08-16 20:29:57 +0200

    Add ICDCode list

---
+++ src/ICDCode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NumNum\UBL;
+
+/**
+ * All possible ICD Identificiation Codes that can be used
+ * To extend, see also: https://docs.peppol.eu/poacc/billing/3.0/codelist/ICD/
+ */
+class ICDCode
+{
+    const NATO_COMMERCIAL_AND_GOV_ENTITY_SYSTEM = "0141";
+    const LUXEMBOURG_CP_CPS_INDEX = "0171";
+    const UBL_BE_PARTY_IDENTIFIER = "0193";
+}
