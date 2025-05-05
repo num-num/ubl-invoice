b25afcc9ffc7bb49fb19a63b0114ba10cdb470cb
Bert Devriese <bert@numnum.be>
2025-05-05 13:50:27 +0200

    Additional fixes when reading CreditNotes

--- tests/Read/ReadUbl.php
+++ tests/Read/ReadUbl.php
@@ -2,6 +2,7 @@
 
 namespace NumNum\UBL\Tests\Read;
 
+use NumNum\UBL\CreditNote;
 use NumNum\UBL\Invoice;
 use PHPUnit\Framework\TestCase;
 
@@ -22,6 +23,20 @@ class ReadUbl extends TestCase
     }
 
     /**
+     * @test
+     * @dataProvider creditNoteFileDataProvider
+     */
+    public function testIfCreditNoteUblCanBeRead($fileName)
+    {
+        $ublReader = \NumNum\UBL\Reader::ubl();
+
+        $invoice = $ublReader->parse(file_get_contents($fileName));
+
+        $this->assertNotNull($invoice);
+        $this->assertInstanceOf(CreditNote::class, $invoice);
+    }
+
+    /**
      * Data provider for testIfInvoiceUblCanBeRead
      */
     public function invoiceFileDataProvider(): array
@@ -30,6 +45,16 @@ class ReadUbl extends TestCase
             [dirname(__DIR__).'/Files/UBL-Invoice-2.1-Example.xml'],
             [dirname(__DIR__).'/Files/UBL-Invoice-2.0-Detached.xml'],
             [dirname(__DIR__).'/Files/ubl-invoice-simple.xml'],
+        ];
+    }
+
+    /**
+     * Data provider for testIfInvoiceUblCanBeRead
+     */
+    public function creditNoteFileDataProvider(): array
+    {
+        return [
+            [dirname(__DIR__).'/Files/UBL-CreditNote.xml'],
         ];
     }
 }
