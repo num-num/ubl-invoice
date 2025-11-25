ce7622425055461d023c7111ed31488963276817
RoanB <77972728+RoanB@users.noreply.github.com>
2025-11-25 13:58:38 +0100

    Implement xmlDeserialize method in CreditNote class
    Add xmlDeserialize method, so that we fill in InvoiceLines with cac CreditNoteLine, if we have a creditNote.

--- src/CreditNote.php
+++ src/CreditNote.php
@@ -2,7 +2,12 @@
 
 namespace NumNum\UBL;
 
+use Sabre\Xml\Reader;
+use Sabre\Xml\XmlDeserializable;
 use Sabre\Xml\XmlSerializable;
+use Doctrine\Common\Collections\ArrayCollection;
+
+use function Sabre\Xml\Deserializer\mixedContent;
 
 class CreditNote extends Invoice implements XmlSerializable
 {
@@ -25,5 +30,19 @@ class CreditNote extends Invoice implements XmlSerializable
     {
         $this->invoiceLines = $creditNoteLines;
         return $this;
+    }
+
+    /**
+     * The xmlDeserialize method is called during xml reading.
+     * @param Reader $xml
+     * @return static
+     */
+    public static function xmlDeserialize(Reader $reader)
+    {
+        $mixedContent = mixedContent($reader);
+        $collection = new ArrayCollection($mixedContent);
+
+        return (static::deserializedTag($mixedContent))
+            ->setCreditNoteLines(ReaderHelper::getArrayValue(Schema::CAC . 'CreditNoteLine', $collection));
     }
 }
